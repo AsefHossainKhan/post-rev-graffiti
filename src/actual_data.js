f828318475d8257901d02adc359de3fa0b313859
Asef Hossain Khan <asefhossain123@gmail.com>
2024-08-29 12:46:58 +0600

    initial dataset

---
+++ src/actual_data.js
@@ -0,0 +1,41 @@
+const graffitiImages = [
+  {
+    id: "0",
+    location: "Bashtola, Nurerchala Rd, Dhaka 1212",
+    district: "Dhaka",
+    photoCredit: "Asef Hossain Khan",
+    artists: "Unknown",
+    description: "Foolish acts done by the then government",
+    url: "/graffiti_images/binodon_bhandar.JPEG",
+  },
+  {
+    id: "1",
+    location: "Bashtola, Nurerchala Rd, Dhaka 1212",
+    district: "Dhaka",
+    photoCredit: "Asef Hossain Khan",
+    artists: "Unknown",
+    description:
+      "Encouraging political discussion in tongs, which would otherwise be avoided by the young generation",
+    url: "/graffiti_images/tong.JPEG",
+  },
+  {
+    id: "2",
+    location: "Tejgaon, Dhaka",
+    district: "Dhaka",
+    photoCredit: "Ashfaq Hossain Khan",
+    artists: "Unknown",
+    description: "Different significant events of the revolution",
+    url: "/graffiti_images/different_events.jpeg",
+  },
+  {
+    id: "3",
+    location: "Tejgaon, Dhaka",
+    district: "Dhaka",
+    photoCredit: "Ashfaq Hossain Khan",
+    artists: "Unknown",
+    description: "Beautiful portrayal of unity ",
+    url: "/graffiti_images/pata_chera_nished.jpeg",
+  },
+];
+
+export default graffitiImages;
