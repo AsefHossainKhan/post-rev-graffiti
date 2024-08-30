6fffeb1e355225b77b667d33284f14d691326d48
Md. Muzammel Hossain Chisty <muzammel.chisty@bjitgroup.com>
2024-08-30 14:14:03 +0600

    Loader

--- src/components/ImageComponent/ImageComponent.jsx
+++ src/components/ImageComponent/ImageComponent.jsx
@@ -1,20 +1,33 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "./ImageComponent.css";
+import ImagePlaceholder from "../../assets/loader2.webp";
+import { useState } from "react";
 const ImageComponent = ({ image, setSelectedImage }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
   const handleImageClick = (image) => {
     setSelectedImage(image);
   };
 
   return (
-    <div className="image-container" onClick={() => handleImageClick(image)}>
+    <div className={`image-container`} onClick={() => handleImageClick(image)}>
       <LazyLoadImage
-        className="image-item"
+        className={`image-item ${isLoaded ? "show" : "hide"}`}
         src={image.url}
         alt={image.description}
+        visibleByDefault={true}
+        onLoad={() => {
+          setIsLoaded(true);
+        }}
       />
-      <div className="image-subtext-container">
+      <div
+        className={`image-subtext-container ${isLoaded ? "show-flex" : "hide"}`}
+      >
         <h1 className="image-subtext">{image.district}</h1>
       </div>
+      <img
+        className={`image-item placeholder ${isLoaded ? "hide" : "show"}`}
+        src={ImagePlaceholder}
+      ></img>
     </div>
   );
 };
